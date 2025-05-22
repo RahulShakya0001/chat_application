4798956584a4c45bd1379195986ed627c156b544
LoginPage <rahulshakya7685@gmail.com>
2025-05-22 12:25:51 +0530

    SetUp

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Auth from './pages/auth'
+import Chat from './pages/chat'
+import Profile from './pages/profile'
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/auth' element={<Auth />} />
+          <Route path='/chat' element={<Chat />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path="*" element={ <Navigate to="/auth" replace /> }></Route>
+        </Routes>
+      </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App
