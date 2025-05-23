ca1a74f47e63afccf920a04533a4a212b60e78ee
LoginPage <rahulshakya7685@gmail.com>
2025-05-23 17:02:19 +0530

    Add react-icons dependency and enhance Profile component layout

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,15 +1,33 @@
-import { useAppStore } from "@/store"
+import { useAppStore } from "@/store";
+import { useState } from "react";
+import { useFormState } from "react-dom";
+import { useNavigate } from "react-router-dom";
+import { IoArrowBack } from "react-icons/io5";
 
 const Profile = () => {
-    const {userInfo} = useAppStore()
-    return (
-        <div>Profile
-            Email: {userInfo.email}
-            <br />
-            Id: {userInfo.id}
-        </div>
-    )
-}
+  const navigate = useNavigate();
+  const { userInfo, userUserInfo } = useAppStore();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [image, setImage] = useState(null);
+  const [hovered, setHovered] = useState(false);
+  const [selectedColor, setSelectedColor] = useState(0);
 
-export default Profile
+  const saveChanges = async () => {};
+  return (
+    <div className="bg-[#1b1c24] h-[100vh] flex items-center flex-col gap-10">
+      <div className="flex flex-col gap-10 w-[80vw] md:w-max">
+        <IoArrowBack className="text-4xl lg:text-6xl text-white/90 cursor-pointer" />
+      </div>
+      <div className="grid grid-cols-2">
+        <div
+          className="h-full w-32 md:w-48 md:h-48 relative flex items-center justify-center"
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+        ></div>
+      </div>
+    </div>
+  );
+};
 
+export default Profile;
