c1f28061b07365ade30e01447609fd1da2714e9e
LoginPage <rahulshakya7685@gmail.com>
2025-05-26 17:34:13 +0530

    sdf

--- client/src/pages/chat/components/chat-container/components/message-bar/index.jsx
+++ client/src/pages/chat/components/chat-container/components/message-bar/index.jsx
@@ -1,11 +1,15 @@
+import { useSocket } from "@/context/SocketContext";
+import { useAppStore } from "@/store";
 import EmojiPicker from "emoji-picker-react";
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, use } from "react";
 import { GrAttachment } from "react-icons/gr";
 import { IoSend } from "react-icons/io5";
 import { RiEmojiStickerLine } from "react-icons/ri";
 
 function MessageBar() {
   const emojiRef = useRef();
+  const socket = useSocket();
+  const {selectedChatType, selectedChatData, userInfo} = useAppStore()
   const [emojiPickerOpen, setEmojiPickerOpen] = useState(false);
   const [message, setMessage] = useState("");
 
@@ -25,7 +29,17 @@ function MessageBar() {
     setMessage((msg) => msg + emoji.emoji);
   };
 
-  const handleSendMessage = async () => {};
+  const handleSendMessage = async () => {
+    if(selectedChatType === "contact") {
+      socket.emit("sendMessage", {
+        sender: userInfo.id,
+        content: message,
+        recipient: selectedChatData._id, 
+        messageTypes: "text",
+        fileUrl: undefined
+      });
+    }
+  };
 
   return (
     <div className="h-[10vh] bg-[#1c1d25] flex justify-center items-center px-8 mb-6 gap-6">
--- server/socket.js
+++ server/socket.js
@@ -1,4 +1,5 @@
 import { Server as SocketIoServer } from "socket.io";
+import Message from "./models/MessageModel.js";
 const setupSocket = (server) => {
     const io = new SocketIoServer(server, {
         cors: {
@@ -8,6 +9,7 @@ const setupSocket = (server) => {
         }
     });
     const userSocketMap = new Map();
+
     const disconnect = (socket) =>{
         console.log(`Client Disconnected: ${socket.id}`);
         for(const [userId, socketId] of userSocketMap.entries() ){
@@ -16,19 +18,38 @@ const setupSocket = (server) => {
                 break;
             }
         }
-    }
-    io.on("connection", (socket) => {
-
-        const userId = socket.handshake.query.userId;
-        if (userId) {
-            userSocketMap.set(userId, socket.id);
-            console.log(`User Connected ${userId} with socket ID: ${socket.id}`);
-        } else {
-            console.log("User ID not provided during connection.")
+    };
+    const sendMessage = async (message) => {
+        const senderSocketId = userSocketMap.get(message.sender);
+        const recipientSocketId = userSocketMap.get(message.recipient);
+        const createMessage = await Message.create(message);
+        const messageData = await Message.findById(createMessage._id)
+            .populate("sender", "id firstName lastName image color")
+            .populate("recipient", "id firstName lastName image color")
+        if(recipientSocketId){
+            io.to(recipientSocketId).emit("recieveMessage", messageData);
         }
+        if(senderSocketId){
+            io.to(senderSocketId).emit("recieveMessage", messageData)
+        }
+    }
+ io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId;
 
-        socket.on("disconnect", () => disconnect(socket))
-    })
+    if (userId) {
+        userSocketMap.set(userId, socket.id);
+        console.log(`User Connected ${userId} with socket ID: ${socket.id}`);
+    } else {
+        console.log("User ID not provided during connection.");
+    }
+
+    socket.on("sendMessage", async (message) => {
+        await sendMessage(message);
+    });
+
+    socket.on("disconnect", () => disconnect(socket));
+});
+
 
 }
 
